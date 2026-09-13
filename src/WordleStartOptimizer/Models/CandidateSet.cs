11d1c154bd708e92b86bfb93420c65f5ee83625a
Liamth99 <liamth@live.com.au>
2026-09-13 20:54:12 +1000

    Refactor `CandidateSet` to remove `PreScore` and optimize index storage using dedicated fields

--- src/WordleStartOptimizer/Models/CandidateSet.cs
+++ src/WordleStartOptimizer/Models/CandidateSet.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 
 namespace WordleStartOptimizer.Models;
 
@@ -8,23 +7,35 @@ public readonly struct CandidateSet
 {
     private string DisplayString => string.Join(", ", WordIndexes().Select(x => Data.ValidGuesses[x]));
 
-    [SetsRequiredMembers]
-    public CandidateSet(IEnumerable<short> indexes, double preScore)
+    public CandidateSet(IEnumerable<short> indexes)
     {
         var indexArr = indexes as short[] ?? indexes.ToArray();
-        PreScore         = preScore;
-        AggregatedIndexes = 0;
 
-        for (int i = 0; i < 8; i++)
-        {
-            if(i < indexArr.Length)
-                AggregatedIndexes |= (Int128)indexArr[i] << (i * 16);
-            else
-                AggregatedIndexes |= (Int128)short.MaxValue << (i * 16);
-        }
+        _index1 = indexArr.Length >= 1 ? indexArr[0] : short.MaxValue;
+        _index2 = indexArr.Length >= 2 ? indexArr[1] : short.MaxValue;
+        _index3 = indexArr.Length >= 3 ? indexArr[2] : short.MaxValue;
+        _index4 = indexArr.Length >= 4 ? indexArr[3] : short.MaxValue;
+        _index5 = indexArr.Length >= 5 ? indexArr[4] : short.MaxValue;
     }
 
-    public short WordIndex(int i) => (short)(AggregatedIndexes >> i * 16);
+    public short WordIndex(int i)
+    {
+        switch (i)
+        {
+            case 0:
+                return _index1;
+            case 1:
+                return _index2;
+            case 2:
+                return _index3;
+            case 3:
+                return _index4;
+            case 4:
+                return _index5;
+            default:
+                throw new IndexOutOfRangeException();
+        }
+    }
 
     public short[] WordIndexes()
     {
@@ -42,6 +53,9 @@ public readonly struct CandidateSet
         return results.ToArray();
     }
 
-    public required Int128 AggregatedIndexes { get; init; }
-    public required double PreScore { get; init; }
+    private readonly short _index1;
+    private readonly short _index2;
+    private readonly short _index3;
+    private readonly short _index4;
+    private readonly short _index5;
 }
