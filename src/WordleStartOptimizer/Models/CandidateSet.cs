86ee33d8246c17a3d8a8e64b2b5eba9236b2a443
Liam Thompson <Liamth@live.com.au>
2026-09-20 08:54:18 +1000

    Fix indexing exxception for CandidateSet.cs (#17)

--- src/WordleStartOptimizer/Models/CandidateSet.cs
+++ src/WordleStartOptimizer/Models/CandidateSet.cs
@@ -40,7 +40,7 @@ public readonly struct CandidateSet
     public short[] WordIndexes()
     {
         List<short> results = new (5);
-        for (int i = 0; i < 8; i++)
+        for (int i = 0; i < 5; i++)
         {
             var index = WordIndex(i);
 
